63a6940ce1ed08bbba6520945b30a31bd18f3e70
Rbel12b <rbel12b@gmail.com>
2025-06-05 17:30:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# HuskyLens
+An extension for [SAM Scratch](https://scratch.samlabs.com/)
+
+This extension adds support for the HuskyLens AI camera, connected to a micro:bit.
+
+Micro:bit firmware: [binary.hex](dist/binary.hex)
---
+++ src/vm/extensions/block/huskylens.js
@@ -0,0 +1,111 @@
+import BlockType from '../../extension-support/block-type';
+import ArgumentType from '../../extension-support/argument-type';
+import translations from './translations.json';
+
+/**
+ * Formatter which is used for translation.
+ * This will be replaced which is used in the runtime.
+ * @param {object} messageData - format-message object
+ * @returns {string} - message for the locale
+ */
+let formatMessage = messageData => messageData.default;
+
+/**
+ * Setup format-message for this extension.
+ */
+const setupTranslations = () => {
+    const localeSetup = formatMessage.setup();
+    if (localeSetup && localeSetup.translations[localeSetup.locale]) {
+        Object.assign(
+            localeSetup.translations[localeSetup.locale],
+            translations[localeSetup.locale]
+        );
+    }
+};
+
+const EXTENSION_ID = 'huskylens';
+
+/**
+ * Scratch 3.0 blocks for example of Xcratch.
+ */
+class ExtensionBlocks {
+    /**
+     * A translation object which is used in this class.
+     * @param {FormatObject} formatter - translation object
+     */
+    static set formatMessage (formatter) {
+        formatMessage = formatter;
+        if (formatMessage) setupTranslations();
+    }
+
+    /**
+     * @return {string} - the name of this extension.
+     */
+    static get EXTENSION_NAME () {
+        return formatMessage({
+            id: 'huskylens.name',
+            default: 'HuskyLens'
+        });
+    }
+
+    /**
+     * @return {string} - the ID of this extension.
+     */
+    static get EXTENSION_ID () {
+        return EXTENSION_ID;
+    }
+
+    /**
+     * URL to get this extension.
+     * @type {string}
+     */
+    static get extensionURL () {
+        return "";
+    }
+
+    /**
+     * Set URL to get this extension.
+     * The extensionURL will be changed to the URL of the loading server.
+     * @param {string} url - URL
+     */
+    static set extensionURL (url) {
+        extensionURL = url;
+    }
+
+    /**
+     * Construct a set of blocks for SAM Labs.
+     * @param {Runtime} runtime - the Scratch 3.0 runtime.
+     */
+    constructor (runtime) {
+        /**
+         * The Scratch 3.0 runtime.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+
+        if (runtime.formatMessage) {
+            // Replace 'formatMessage' to a formatter which is used in the runtime.
+            formatMessage = runtime.formatMessage;
+        }
+        this.extensionId = 'huskylens';
+    }
+
+    /**
+     * @returns {object} metadata for this extension and its blocks.
+     */
+    getInfo () {
+        setupTranslations();
+        return {
+            id: ExtensionBlocks.EXTENSION_ID,
+            name: ExtensionBlocks.EXTENSION_NAME,
+            extensionURL: ExtensionBlocks.extensionURL,
+            showStatusButton: false,
+            color1: '#e7660b',
+            color2: '#e7660b',
+            blocks: [],
+            menus: {}
+        };
+    }
+}
+
+export {ExtensionBlocks as default, ExtensionBlocks as blockClass};
